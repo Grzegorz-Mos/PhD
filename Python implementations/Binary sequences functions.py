21476e3abd17134e8000ccbcd985e7a8c12bda35
Grzegorz-Mos <grzegorz.mos@us.edu.pl>
2025-09-21 11:17:05 +0200

    Update Binary sequences functions.py

--- Python implementations/Binary sequences functions.py
+++ Python implementations/Binary sequences functions.py
@@ -1,3 +1,6 @@
+import numba
+import time
+
 def tauStr( binarySequenceString: str ) -> str:
 	return ''.join('1' if b == '0' else '0' for b in binarySequenceString)
 
@@ -24,3 +27,24 @@ def piStr( binarySequenceString: str ) -> str:
 def piInt( binarySequenceInteger: int , length: int ) -> int:
   return sigmaInt( tauInt( binarySequenceInteger , length ) , length )
 
+
+
+# Comparison of speed for each pi implementation
+length = 25
+binarySequencesInts = range( 2 ** length )
+binarySequencesStrs = [
+    format( binarySequenceInteger , f'0{length}b' )
+    for binarySequenceInteger in binarySequencesInts
+]
+
+start = time.time()
+for binarySequenceInteger in binarySequencesInts:
+  piInt( binarySequenceInteger , length )
+elapsed_time = time.time() - start
+print(f"Time: {elapsed_time:.3f} seconds")
+
+start = time.time()
+for binarySequenceString in binarySequencesStrs:
+  piStr( binarySequenceString )
+elapsed_time = time.time() - start
+print(f"Time: {elapsed_time:.3f} seconds")
